3aa7d280b3f400729b30a85e9befa5f24a710081
Francis Tseng <ftzeng@gmail.com>
2013-12-30 17:25:21 -0500

    reverting

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 from os import environ as env
 
-app = Flask(__name__, static_folder='static', static_url_path='/')
+app = Flask(__name__, static_folder='static', static_url_path='')
 
 # Load config.
 if not env.get('HEROKU'):
